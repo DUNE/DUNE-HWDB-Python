2da57db3bf0b5d9c177ff50b3cf46f6f78b36750
wagn0033 <98336731+wagn0033@users.noreply.github.com>
2025-03-27 09:51:19 -0500

    updated version

--- lib/Sisyphus/__init__.py
+++ lib/Sisyphus/__init__.py
@@ -8,7 +8,7 @@ Author: Alex Wagner <wagn0033@umn.edu>, Dept. of Physics and Astronomy
 
 import os
 
-version = 'v1.2.3.dev.2025.03.21a'
+version = 'v1.2.4.rel.2025.03.27a'
 
 project_root = os.path.realpath(os.path.join(os.path.dirname(__file__), "../.."))
 
